1360b500b71c9dcb0771b5a2e988cbda392a3de3
oskarmozdzen <oskar.mozdzen@daw-systems.pl>
2024-10-01 15:56:37 +0200

    fix

--- app/src/main/java/pl/gotrainey/gotrainey/adapters/StationAdapter.kt
+++ app/src/main/java/pl/gotrainey/gotrainey/adapters/StationAdapter.kt
@@ -13,19 +13,16 @@ import com.google.gson.JsonObject
 class StationAdapter(context: Context, resource: Int, private val originalItems: List<String>) :
     ArrayAdapter<String>(context, resource, originalItems) {
 
-    // This method is not used for filtering but just returns the original items
     override fun getFilter(): Filter {
         return object : Filter() {
             override fun performFiltering(constraint: CharSequence?): FilterResults {
-                // Skip filtering and return all items
                 val results = FilterResults()
-                results.values = originalItems // Return the original list
+                results.values = originalItems
                 results.count = originalItems.size
                 return results
             }
 
             override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
-                // No action needed; we are not filtering
                 if (results != null && results.count > 0) {
                     clear()
                     addAll(results.values as List<String>)
